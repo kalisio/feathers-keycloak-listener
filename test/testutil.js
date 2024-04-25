67480fb82027d01ba2339bbe0f9f64f034eed1ad
David Andrianavalontsalama <david.andriana@avantage-compris.com>
2024-04-25 15:10:15 +0200

    tests: Use mocha Context to save screenshots in separate directories given by describe

--- test/testutil.js
+++ test/testutil.js
@@ -12,7 +12,17 @@ export const driver = new webdriver.Builder()
 	.withCapabilities(webdriver.Capabilities.firefox()) // Uses RemoteWebDriver
 	.build();
 
-export const ready = () => Promise.resolve();
+export const context = {
+
+	ready: () => Promise.resolve(),
+
+	set: function(mochaContext) {
+	
+		this.mochaContext = mochaContext;
+	},
+	
+	mochaContext: null,
+};
 
 export const intent = (message) => () => new Promise((resolve, reject) => {
 	console.log('    intent: %s', message);
@@ -24,14 +34,20 @@ var screenshotCount = 0;
 export const takeScreenshotAndIncreaseCounter = () => new Promise((resolve, reject) => {
 
 	driver.takeScreenshot().then((data) => {
-
+	
 		++screenshotCount;
 		const fileName = screenshotCount.toString().padStart(8, '0') + '.png';
 		console.log('      -> screenshot: %s', fileName);
 		if (!fs.existsSync('screenshots')) {
 			fs.mkdirSync('screenshots');
 		}
-		fs.writeFileSync('screenshots/' + fileName, data, 'base64', (error) => {
+		const screenshotsDir = (context.mochaContext === null)
+			? 'screenshots'
+			: ('screenshots/' + context.mochaContext.test.parent.title);
+		if (!fs.existsSync(screenshotsDir)) {
+			fs.mkdirSync(screenshotsDir);
+		}
+		fs.writeFileSync(screenshotsDir + '/' + fileName, data, 'base64', (error) => {
 			if (error) {
 				console.log(error);
 				assert.fail('While taking screenshot: ' + fileName);
